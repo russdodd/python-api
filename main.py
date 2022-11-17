44d6535e00e6fc5cbbd07f2b9ae4a6cd1294adf4
Russell Dodd <russell.dodd15@gmail.com>
2022-11-17 10:57:26 -0500

    add otel trace

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # main.py
 
 from fastapi import FastAPI
+from opentelemetry import trace
+
+tracer = trace.get_tracer(__name__)
 
 app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
-    return {"message": "Hello World", "item": item_id}
+    return read_item(item_id)
+
+def read_item(item_id: int) -> dict:
+    with tracer.start_as_current_span("read_item") as item_span:
+        item_span.set_attribute("item.id", item_id)
+        return {"message": "Hello World", "item": item_id}
