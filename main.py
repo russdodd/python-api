ae281c65068b96655e6ea666612fa505d1f3f6e5
Russell Dodd <russell.dodd15@gmail.com>
2022-11-17 14:45:22 -0500

    adjust increase amount for metric

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def read_item(item_id: int) -> dict:
     with tracer.start_as_current_span("read_item") as item_span:
         item_span.set_attribute("item.id", item_id)
         wait_a_little()
-        item_request_counter.add(1, {"item.id": item_id})
+        item_request_counter.add(2, {"item.id": item_id})
         return {"message": "Hello World", "item": item_id}
 
 def wait_a_little():
