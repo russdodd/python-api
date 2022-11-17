b20dbc0c6ce27beb5cac9c0913fc6c01da537867
Russell Dodd <russell.dodd15@gmail.com>
2022-11-16 21:54:51 -0500

    update endpoint path

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"message": "Hello World", "item": item_id}
